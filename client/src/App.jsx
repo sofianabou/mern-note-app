8231882506355bd5f10d30638c13f23e44136895
sofiane <sofyahdoc@gmail.com>
2024-03-06 16:43:09 +0100

    installation react+tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Home from "./pages/Home";
+import Notes from "./pages/Notes";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./component/Navbar";
+import { Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./utils/PrivateRoutes";
+
+const App = () => {
+
+  return (
+    <>
+    <Navbar />
+    <Routes >
+      <Route element={<PrivateRoutes />}>
+        <Route path="/notes" element={<Notes />} />
+      </Route>
+     <Route path="/" element={<Home />} />
+     <Route path="/register" element={<Register />} />
+     <Route path="/login" element={<Login />} />
+     </Routes>
+    </>
+  )
+ 
+};
+
+export default App
